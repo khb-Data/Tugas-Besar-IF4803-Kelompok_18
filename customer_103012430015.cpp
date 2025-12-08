04ba95e90984c86124b864aa2c50ba94421bb59b
pidakbar <hafidzakbar0614@gmail.com>
2025-12-08 16:27:13 +0700

    menambahkan insertCostumerLast

---
+++ customer_103012430015.cpp
@@ -0,0 +1,18 @@
+#include "customer.h"
+#include <iostream>
+using namespace std;
+
+void insertCustomerLast(ListCustomer &LC, adrCustomer C){
+    if (isEmptyCustomer(LC)){
+        LC.first = C;
+        LC.last = C;
+    }else {
+        C->prev = LC.last;
+        LC.last->next = C;
+        LC.last = C;
+    }
+}
+
+void insertCustomerSorted(ListCustomer &LC, adrCustomer C){
+
+}
